b344ede97b5049f39370607d22ccf9daca4d04eb
Tim Arnold <jtim.arnold@gmail.com>
2014-03-16 14:52:32 -0400

    added argparse, cleaned up splitting logic.

--- odspdfcrop.py
+++ odspdfcrop.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import defaultdict
 from multiprocessing import Process, Queue
 import os
@@ -9,8 +10,6 @@ import time
 from pyPdf import PdfFileReader, PdfFileWriter
 from pyPdf.generic import NameObject, createStringObject
 
-
-GS = 'c:\\path\\bin\\gswin64c '
 pat = re.compile(r'([\w_\d]+?)(\d+)\.pdf')
 
 def get_filedigit(fdict):
@@ -42,8 +41,8 @@ def write_page(filename, obj, pagenum, crop=False):
     p.addPage(page)
     p.write(open(filename, 'wb'))
 
-def get_bbox(filename):
-    cmd = '%s -sDEVICE=bbox -dBATCH -dNOPAUSE  -dQUIET %s' % (GS, filename)
+def get_bbox(ghostscript, filename):
+    cmd = '%s -sDEVICE=bbox -dBATCH -dNOPAUSE  -dQUIET %s' % (ghostscript, filename)
     s = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     s = s[s.rindex('HiResBoundingBox:') + 17:].strip()
     return s.split()
@@ -58,8 +57,10 @@ def rename_files(source_dir, extstring):
             os.unlink(src)
 
 class PDFFixer(object):
-    def __init__(self, source_dir):
-        self.source_dir = source_dir
+    def __init__(self, args):
+        self.source_dir = args.dir
+        self.ghostscript = args.ghostscript
+
         self.file_info = list()
         self.cropped = list()
         self.pdffiles = [x for x in os.listdir(self.source_dir) if x.endswith('.pdf')]
@@ -73,7 +74,7 @@ class PDFFixer(object):
 
         for _ in processes:
             item = q.get()
-            fname, cropped, pages = item['fname'], item['stem'], item['cropped'], item['pages']
+            fname, cropped, pages = item['fname'], item['cropped'], item['pages']
             self.file_info.append({'fname': fname, 'pages': pages})
             if cropped:
                 self.cropped.append(fname)
@@ -101,25 +102,34 @@ class PDFFixer(object):
             if sum(x['pages'] for x in stem_info[stem]) == len(stem_info[stem]):
                 continue
 
-            for pdfdict in sorted(stem_info, key=get_filedigit):
+            start_splitting = False
+            filedigit = 0
+            for pdfdict in sorted(stem_info[stem], key=get_filedigit):
                 fname = pdfdict['fname']
                 pages = pdfdict['pages']
-                print '%30s (%d pages)' % (fname, pages)
 
+                if not start_splitting and pages > 1:
+                    start_splitting = True
+                if not start_splitting:
+                    filedigit += 1
+                    continue
+
+                print '%30s (%d pages)' % (fname, pages)
                 obj = PdfFileReader(open(os.path.join(self.source_dir, fname), 'rb'))
-                c = 0
+
                 for pagenum in range(0, pages):
-                    if c == 0:
+                    if filedigit == 0:
                         fname = os.path.join(self.source_dir, '%s_SPLIT.pdf' % stem)
                         rname = '%s.pdf' % stem
                     else:
-                        fname = os.path.join(self.source_dir, '%s%d_SPLIT.pdf' % (stem, c))
-                        rname = '%s%d.pdf' % (stem, c)
+                        fname = os.path.join(self.source_dir, '%s%d_SPLIT.pdf' % (stem, filedigit))
+                        rname = '%s%d.pdf' % (stem, filedigit)
                     write_page(fname, obj, pagenum)
 
                     if self.cropped.count(rname):
                         self.cropped.remove(rname)
-                    c += 1
+                    filedigit += 1
+
                 obj.stream.close()
 
         rename_files(self.source_dir, '_SPLIT.pdf')
@@ -143,23 +153,32 @@ class PDFFixer(object):
 
     def crop_process(self, name):
         fullname = os.path.join(self.source_dir, name)
-        basename = os.path.basename(fullname)
-        newstem = os.path.splitext(basename)[0]
         obj = PdfFileReader(open(fullname, 'rb'))
 
-        lx, ly, ux, uy = get_bbox(fullname)
         print '+',
+        lx, ly, ux, uy = get_bbox(self.ghostscript, fullname)
         page = obj.getPage(0)
         page.mediaBox.lowerLeft = lx, ly
         page.mediaBox.lowerRight = ux, ly
         page.mediaBox.upperLeft = lx, uy
         page.mediaBox.upperRight = ux, uy
-        write_page(os.path.join(self.source_dir, '%s_CROP.pdf' % newstem), obj, 0, crop=True)
 
-if __name__ == '__main__':
+        new_name = os.path.join(self.source_dir, '%s_CROP.pdf' % os.path.splitext(name)[0])
+        write_page(new_name, obj, 0, crop=True)
+
+def main(args):
     t0 = time.clock()
-    source_dir = 'c:/pdf'
-    f = PDFFixer(source_dir)
-    f.split()
+    f = PDFFixer(args)
+    if not args.nosplit:
+        f.split()
+
     f.crop()
     print 'Finished: ', time.clock() - t0, ' processing seconds'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', default=os.path.join(os.getcwd(), 'pdf'))
+    parser.add_argument('--nosplit', action='store_true')
+    parser.add_argument('--ghostscript', default='c:\\bin\\gswin64c ')
+    args = parser.parse_args()
+    main(args)
